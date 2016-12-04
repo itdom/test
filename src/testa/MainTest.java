bc7cae6d5967d81b1741fcdb7c9accf66ba1425e
高德龙 <@DESKTOP-21LAMR0>
2016-12-04 11:01:19 +0800

    o

--- src/testa/MainTest.java
+++ src/testa/MainTest.java
@@ -3,7 +3,7 @@ package testa;
 public class MainTest {
 
     public static void main(String[] args){
-        System.out.print("master 3");
+        System.out.print("master o");
         System.out.print("master 1");
     }
 }
