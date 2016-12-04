96bb028d31855a53d72dcd86c4773eee4ff00fc7
高德龙 <dgaodelong@163.com>
2016-12-04 12:09:40 +0800

    1

--- src/testa/MainTest.java
+++ src/testa/MainTest.java
@@ -5,6 +5,5 @@ public class MainTest {
     public static void main(String[] args){
         System.out.print("master 3");
         System.out.print("master 1");
-        System.out.print("master 2");
     }
 }
